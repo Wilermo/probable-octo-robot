0de36dbe957d0ac25ae449ac99ac531525443ef4
Valentina Hernandez Bayona <valenh20023008@gmail.com>
2024-04-10 03:21:09 -0500

    1.2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { Landing1Component } from './components/landing1/landing1.component';
 import { FormsModule } from '@angular/forms';
-import { LoginRRHHComponent } from './components/login-rrhh/login-rrhh.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { Navbar2Component } from './components/navbar2/navbar2.component';
 import { Footer2Component } from './components/footer2/footer2.component';
 import { PaymentComponent } from './components/payment/payment.component';
-import { DashboardHRComponent } from './components/dashboard-hr/dashboard-hr.component';
 import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
@@ -19,12 +17,10 @@ import {HttpClientModule} from "@angular/common/http";
     AppComponent,
     NavbarComponent,
     Landing1Component,
-    LoginRRHHComponent,
     FooterComponent,
     Navbar2Component,
     Footer2Component,
     PaymentComponent,
-    DashboardHRComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlanServiceService } from 'src/app/service/plan-service.service';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.css']
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
+  planes: any[] = [];
 
+  constructor(private planService: PlanServiceService) { }
+
+  ngOnInit(): void {
+    /* this.planService.getPlanes().subscribe(data =>{
+       this.planes = data;
+   });*/
+
+   //Esto era para pruebas mías, pero cuando vayas a hacer lo del back borra esto y descomenta lo de arriba
+    this.planes = [
+      { name: 'Plan 1', price: '$30.00', description: 'Perfecto para empresas en fase de exploración. Accede a nuestra plataforma durante 3 meses para implementar y evaluar si TalentSoft se ajusta a tus necesidades.', feature1: 'Característica 1', feature2: 'Característica 2', feature3: 'Característica 3' },
+      { name: 'Plan 2', price: '$60.00', description: 'Perfecto para empresas en fase de exploración. Accede a nuestra plataforma durante 3 meses para implementar y evaluar si TalentSoft se ajusta a tus necesidades.', feature1: 'Característica 1', feature2: 'Característica 2', feature3: 'Característica 3' },
+      { name: 'Plan 3', price: '$30.00', description: 'Perfecto para empresas en fase de exploración. Accede a nuestra plataforma durante 3 meses para implementar y evaluar si TalentSoft se ajusta a tus necesidades.', feature1: 'Característica 1', feature2: 'Característica 2', feature3: 'Característica 3' },
+      { name: 'Plan 4', price: '$60.00', description: 'Perfecto para empresas en fase de exploración. Accede a nuestra plataforma durante 3 meses para implementar y evaluar si TalentSoft se ajusta a tus necesidades.', feature1: 'Característica 1', feature2: 'Característica 2', feature3: 'Característica 3' },
+      { name: 'Plan 5', price: '$90.00', description: 'Perfecto para empresas en fase de exploración. Accede a nuestra plataforma durante 3 meses para implementar y evaluar si TalentSoft se ajusta a tus necesidades.', feature1: 'Característica 1', feature2: 'Característica 2', feature3: 'Característica 3' }
+    ];
+  }
+  chunkArray(arr: any[], chunkSize: number): any[] {
+    const chunkedArray = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      chunkedArray.push(arr.slice(i, i + chunkSize));
+    }
+    return chunkedArray;
+  }
 }
