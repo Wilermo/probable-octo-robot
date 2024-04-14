25a0d7885bf9f869f1ca1aedb753a2902596fc5f
Wilermo <william.dmo.sec@gmail.com>
2024-04-14 15:09:04 -0500

    Conexión con API Gateway canela

--- src/app/service/landing.service.ts
+++ src/app/service/landing.service.ts
@@ -3,7 +3,6 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {FormInfo} from "../model/form-info";
 
-
 @Injectable({
   providedIn: 'root'
 })
