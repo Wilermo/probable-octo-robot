074cb4a44958158d1c699be67d1b231a97dcb9ad
Wilermo <william.dmo.sec@gmail.com>
2024-04-17 18:21:43 -0500

    URL

--- src/app/model/registrarTarjeta.service.ts
+++ src/app/model/registrarTarjeta.service.ts
@@ -2,22 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { PosibleUsuario } from './Entities/posibleUsuario'; 
+import { PosibleUsuario } from './Entities/posibleUsuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrarTarjetaService {
-  
+
   constructor(private http: HttpClient) { }
 
-  registrarTarjetaConPersona(posibleUsuario: PosibleUsuario): Observable<any> { 
-    const url = `${environment.cardURL}`;
-    
+  registrarTarjetaConPersona(posibleUsuario: PosibleUsuario): Observable<any> {
+    const url = 'https://canelaaccounmanager.up.railway.app/payment/card';
+
     const headers = new HttpHeaders({
       'Content-Type': 'application/json'
     });
 
-    return this.http.post<any>(url, posibleUsuario, { headers }); 
+    return this.http.post<any>(url, posibleUsuario, { headers });
   }
 }
